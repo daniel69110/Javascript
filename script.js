ee007297fbc3b8b4d2ed8e812b259f961b0df547
Daniel-C <danielcourot@hotmail.com>
2025-04-16 14:22:16 +0200

    changement

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 const fenetres = document.querySelector('.fenetre')
+const boutons = document.querySelectorAll('button');
+//Méthode 1
+ 
+// function ajouter(valeur) {
+//     fenetres.textContent += valeur;
+// }
+
+// function effacer() {
+//     fenetres.textContent = '';
+// }
+
+// function calculer() {
+//     try {
+//       fenetres.textContent = eval(fenetres.textContent);
+//     } catch {
+//       fenetres.textContent = 'Erreur';
+//     }
+//   }
+
+// Méthode 2 AddEventListener
 
 
-//afficher les éléments 
-function ajouter(valeur) {
-    fenetres.textContent += valeur;
-}
 
-function effacer() {
-    fenetres.textContent = '';
-}
+boutons.forEach(bouton => {
+  bouton.addEventListener('click', () => {
+    const valeur = bouton.textContent;;
 
-function calculer() {
-    try {
-      fenetres.textContent = eval(fenetres.textContent);
-    } catch {
-      fenetres.textContent = 'Erreur';
+    if (valeur === 'C') {
+      fenetres.textContent = '';
+    } else if (valeur === '=') {
+      try {
+        fenetres.textContent = eval(fenetres.textContent);
+      } catch {
+        fenetres.textContent = 'Erreur';
+      }
+    } else {
+      fenetres.textContent += valeur;
     }
-  }
+  });
+});
